42e75e760f57797a45184d873ab8f0a2de15311d
gogoing-code <1378412050@qq.com>
2021-09-29 21:00:39 +0800

    sentinel集成nacos实现配置的持久化

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/zzg/springcloud/alibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/zzg/springcloud/alibaba/controller/CircleBreakerController.java
@@ -29,7 +29,7 @@ public class CircleBreakerController
     //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
     //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
     @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
-            exceptionsToIgnore = {IllegalArgumentException.class})
+            exceptionsToIgnore = {IllegalArgumentException.class}) //忽略异常，后期人工修复，系统仍然运行
     public CommonResult<Payment> fallback(@PathVariable Long id)
     {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id,CommonResult.class,id);
@@ -42,12 +42,14 @@ public class CircleBreakerController
 
         return result;
     }
-    //本例是fallback
+
+    //本例是fallback,业务异常
     public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
         Payment payment = new Payment(id,"null");
         return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
     }
-    //本例是blockHandler
+
+    //本例是blockHandler，sentinel控制台配置异常
     public CommonResult blockHandler(@PathVariable  Long id,BlockException blockException) {
         Payment payment = new Payment(id,"null");
         return new CommonResult<>(445,"blockHandler-sentinel限流,无此流水: blockException  "+blockException.getMessage(),payment);
